c3cae79d750161863e3a76d4cc4c4a7f35b123df
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2021-09-07 16:33:29 +0200

    sort lines to spot duplicates

--- channels.go
+++ channels.go
@@ -1,20 +1,20 @@
 package main
 
 var channelsIDs = []string{
-	"UCIJXOvggjKtCagMfxvcCzAA", // sky news
-	"UCUHq39HXLyAjjMPIjsNRLpw", // cbc live
-	"UC65F33K2cXk9hGDbOQYhTOw", // extra news
 	"UC30ditU5JI16o5NbFsHde_Q", // DW arabic
-	"UCCv1Pd24oPErw5S7zJWltnQ", // aljazeera mubasher
-	"UCfiwzLy-8yKzIbsmZTzxDgw", // aljazeera
-	"UCahpxixMCwoANAftn6IxkTg", // alarabia
-	"UCelk6aHijZq-GJBBB9YpReA", // bbc arabic
-	"UCdTyuXgmJkG_O8_75eqej-w", // france 24
-	"UCRJUVYt9gjg8MnlGacySUdg", // elsharq news
+	"UC65F33K2cXk9hGDbOQYhTOw", // extra news
 	"UC65i3Fyq91uiUkIS0i5GLNQ", // alaraby
-	"UCsP3Clx2qtH2mNZ6KolVoZQ", // RT arabic
+	"UCCv1Pd24oPErw5S7zJWltnQ", // aljazeera mubasher
 	"UCH4UNKBHSNwstan99YDoCVg", // elghad
-	"UCeY0bbntWzzVIaj2z3QigXg", // NBC news
-	"UCknLrEdhRCp1aegoMqRaCZg", // DW news
+	"UCIJXOvggjKtCagMfxvcCzAA", // sky news
+	"UCRJUVYt9gjg8MnlGacySUdg", // elsharq news
 	"UCSrZ3UV4jOidv8ppoVuvW9Q", // euro news usa
+	"UCUHq39HXLyAjjMPIjsNRLpw", // cbc live
+	"UCahpxixMCwoANAftn6IxkTg", // alarabia
+	"UCdTyuXgmJkG_O8_75eqej-w", // france 24
+	"UCeY0bbntWzzVIaj2z3QigXg", // NBC news
+	"UCelk6aHijZq-GJBBB9YpReA", // bbc arabic
+	"UCfiwzLy-8yKzIbsmZTzxDgw", // aljazeera
+	"UCknLrEdhRCp1aegoMqRaCZg", // DW news
+	"UCsP3Clx2qtH2mNZ6KolVoZQ", // RT arabic
 }
